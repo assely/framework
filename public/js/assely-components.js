c9df7a3439bfcc3e60a3e716e608b58eb852add3
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-01-09 22:28:51 +0100

    Diffrent background for repeatable boxes

--- public/js/assely-components.js
+++ public/js/assely-components.js
@@ -1,1 +1,22 @@
-Vue.component("assely-alert",{template:"#tmpl-assely-alert"}),Vue.component("assely-box",{template:"#tmpl-assely-box",props:["title","description","type","column"]}),Vue.component("assely-fields",{template:"#tmpl-assely-fields",props:["fields","namespace"]});
+Vue.component('assely-alert', {
+    template: '#tmpl-assely-alert'
+});
+Vue.component('assely-box', {
+    template: '#tmpl-assely-box',
+
+    props: [
+        'title',
+        'description',
+        'type',
+        'column'
+    ]
+});
+Vue.component('assely-fields', {
+    template: '#tmpl-assely-fields',
+
+    props: [
+        'fields',
+        'namespace'
+    ]
+});
+//# sourceMappingURL=assely-components.js.map
