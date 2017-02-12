dfce9f40577891b45d53dc68cb1808e9a436169b
Jędrzej Chałubek <jedrzej.chalubek@gmail.com>
2017-02-12 02:55:01 +0100

    Apply fixes from StyleCI (#56)

--- tests/Adapter/AdapterTest.php
+++ tests/Adapter/AdapterTest.php
@@ -4,63 +4,63 @@ use Assely\Adapter\Adapter;
 
 class AdapterTest extends TestCase
 {
-	/**
-	 * @test
-	 */
-	public function it_should_throw_exception_on_accessing_non_existion_property()
-	{
-		$adapter = $this->getAdapter();
+    /**
+     * @test
+     */
+    public function it_should_throw_exception_on_accessing_non_existion_property()
+    {
+        $adapter = $this->getAdapter();
 
-		$this->expectException('Exception');
+        $this->expectException('Exception');
 
-		$adapter->nonExistingProperty;
-	}
+        $adapter->nonExistingProperty;
+    }
 
-	/**
-	 * @test
-	 */
-	public function test_property_getters()
-	{
-		$adapter = $this->getAdapter();
+    /**
+     * @test
+     */
+    public function test_property_getters()
+    {
+        $adapter = $this->getAdapter();
 
-		$this->assertEquals('property', $adapter->property);
-		$this->assertEquals('propertyByMethod', $adapter->propertyByMethod);
-	}
+        $this->assertEquals('property', $adapter->property);
+        $this->assertEquals('propertyByMethod', $adapter->propertyByMethod);
+    }
 
-	/**
-	 * @test
-	 */
-	public function test_property_setters()
-	{
-		$adapter = $this->getAdapter();
+    /**
+     * @test
+     */
+    public function test_property_setters()
+    {
+        $adapter = $this->getAdapter();
 
-		$adapter->property = 'updated-property';
+        $adapter->property = 'updated-property';
 
-		$this->assertEquals('updated-property', $adapter->property);
-	}
+        $this->assertEquals('updated-property', $adapter->property);
+    }
 
-	public function getAdapter()
-	{
-		return new AdapterStub;
-	}
+    public function getAdapter()
+    {
+        return new AdapterStub;
+    }
 }
 
 class AdapterStub extends Adapter
 {
-	public $property = 'property';
+    public $property = 'property';
 
-	public function propertyByMethod()
-	{
-		return 'propertyByMethod';
-	}
+    public function propertyByMethod()
+    {
+        return 'propertyByMethod';
+    }
 
-	public function connect()
-	{
-		//
-	}
+    public function connect()
+    {
+        //
+    }
 
-	public function jsonSerialize()
-	{
-		//
-	}
+    public function jsonSerialize()
+    {
+        //
+    }
 }
